11e4411b83f64e701b5cb32472e99a2e86dc0be7
aman <amanv4775@gmail.com>
2023-05-13 20:21:19 +0530

    added script

--- script.js
+++ script.js
@@ -7,21 +7,47 @@ let arr = [
 ];
 
 function PrintStudentswithMap() {
-  //Write your code here , just console.log
-}
-
-function PrintStudentsbyForEach() {
-  //Write your code here , just console.log
-}
-
-function addData() {
-  //Write your code here, just console.log
-}
-
-function removeFailedStudent() {
-  //Write your code here, just console.log
-}
-
-function concatenateArray() {
-  //Write your code here, just console.log
-}
+    //Write your code here , just console.log
+    let highMarksStudent=arr.map(function(element,index,list){
+        if(arr[index].marks>50){
+            console.log(arr[index]);
+        }
+    });
+  }
+  
+  function PrintStudentsbyForEach() {
+    //Write your code here , just console.log
+       arr.forEach(function(element,index){
+        if(element.marks>50){
+            console.log(arr[index]);
+        }
+       });
+  }
+  
+  function addData() {
+    //Write your code here, just console.log
+        let newStudent = {id:4,name:"susan",age:"20",marks:45};
+        arr.push(newStudent);
+        console.log(arr);
+  }
+  
+  function removeFailedStudent() {
+    //Write your code here, just console.log
+    arr=arr.filter(function(element,index){
+        if(element.marks>50){
+          return arr[index];
+        }
+    });
+    console.log(arr);
+  }
+  
+  function concatenateArray() {
+    //Write your code here, just console.log
+    let newStudents = [
+        {id:5,name:"Anne",age:"18",marks:75},
+        {id:6,name:"David",age:"19",marks:32},
+        {id:7,name:"Emily",age:"20",marks:92}
+      ];
+      arr=arr.concat(newStudents);
+      console.log(arr);
+  }
